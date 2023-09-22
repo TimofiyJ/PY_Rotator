2fd20b7b2dc4878a50abf9522aa41004c1e3067e
Tim <44300490+TimofiyJ@users.noreply.github.com>
2023-09-22 17:09:25 +0300

    works fine

--- correct_skew.py
+++ correct_skew.py
@@ -15,27 +15,31 @@ image = cv2.imread(args["image"])
 # the background is "black"
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 gray = cv2.bitwise_not(gray)
+cv2.imshow("Input", gray)
 # threshold the image, setting all foreground pixels to
 # 255 and all background pixels to 0
 thresh = cv2.threshold(gray, 0, 255,
 	cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-
+print(thresh)
 # grab the (x, y) coordinates of all pixel values that
 # are greater than zero, then use these coordinates to
 # compute a rotated bounding box that contains all
 # coordinates
 coords = np.column_stack(np.where(thresh > 0))
+print(coords)
 angle = cv2.minAreaRect(coords)[-1]
+print(cv2.minAreaRect(coords))
 # the `cv2.minAreaRect` function returns values in the
 # range [-90, 0); as the rectangle rotates clockwise the
 # returned angle trends to 0 -- in this special case we
 # need to add 90 degrees to the angle
-if angle < -45:
-	angle = -(90 + angle)
+if angle > 30:
+	angle = abs(90 - angle)
 # otherwise, just take the inverse of the angle to make
 # it positive
 else:
 	angle = -angle
+
 	
 # rotate the image to deskew it
 (h, w) = image.shape[:2]
